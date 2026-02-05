92f951690f6fa2351a4d4b2956da9aea47668758
samanthavms <samanthav316@gmail.com>
2026-02-04 21:26:37 -0300

    Meu projeto de Programação I

---
+++ agente.py
@@ -0,0 +1,10 @@
+def dados_jogador():
+    return{
+        "posição": [0,0],
+        "vivo": True ,
+        "tem_ouro": False,
+        "flecha": 1,
+        "pontos":100
+    }
+
+def acoes_jogador():
---
+++ mundo.py
@@ -0,0 +1,13 @@
+def criar_mundo():
+# Toda tupla tem que ter Wunpus, Poço, Brisa Fedo e Ouro
+    mapa= [
+        [(False,False,False,False,False),(False,False,True,False,False),(False,True,False,False,False),(False,False,True,False,False)],
+        [(False, False, False,True,False ),(False,False,False,False,False),(False,False,True,False,False),(False,False,False,False,False)],
+        [(True,False,False,False,False),(False,False,False,True,False),(False,False,False,False,False),(False,False,True,False,False)],
+        [(False,False,False,True,False),(False,False,False,False,False),(False,False,True,False,False),(False,False,False,False,True)]
+
+    ]
+return mapa 
+
+def percepcoes_sentidos(mapa):
+linha
